a1277ec8ec2ad4e7dd5dfd49ccdac75858eb6d44
Alysson Victor <developeralysson@gmail.com>
2021-10-06 19:18:37 -0300

    Se aplicado, esse commit vai implementar correções no momento de instanciar as classes Calculadora e Pessoa

--- test/Calculadora.test.js
+++ test/Calculadora.test.js
@@ -2,12 +2,12 @@ let Calculadora = require("../Atividade3/Calculadora.js");
 let Pessoa = require("../Atividade3/Pessoa.js");
 
 describe("Calc", () => {
+	let Calc = new Calculadora();
+
 	test("Abaixo do peso", () => {
-		Pessoa = new Pessoa("Alysson Victor", 50, 1.70);
+		let Pessoa1 = new Pessoa("Alysson Victor", 50, 1.70);
 
-		Calculadora = new Calculadora();
-
-		let IMC = Calculadora.calcular(Pessoa);
+		let IMC = Calc.calcular(Pessoa1);
 
 		expect(IMC.getClassificacao()).toBe("abaixo do peso");
 	});
